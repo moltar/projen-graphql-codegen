1f1a29e8394880429bb99d29948e455652651810
Roman Filippov <rf@romanfilippov.com>
2022-02-14 10:52:00 +0700

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,10 @@
+import { cdk } from "projen";
+const project = new cdk.JsiiProject({
+  author: "Roman Filippov",
+  authorAddress: "rf@romanfilippov.com",
+  defaultReleaseBranch: "main",
+  name: "projen-graphql-codegen",
+  projenrcTs: true,
+  repositoryUrl: "https://github.com/rf/projen-graphql-codegen.git",
+});
+project.synth();
