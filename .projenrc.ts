bd73cf92088cc62740ea29ebd4a941d4613cb719
Roman Filippov <rf@romanfilippov.com>
2022-02-14 17:39:12 +0700

    build: switch to using npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { typescript } from 'projen';
+import { typescript, javascript } from 'projen';
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
@@ -6,13 +6,14 @@ const project = new typescript.TypeScriptProject({
   authorEmail: 'rf@romanfilippov.com',
   repository: 'https://github.com/moltar/projen-graphql-codegen.git',
   name: 'projen-graphql-codegen',
+  packageManager: javascript.NodePackageManager.NPM,
   projenrcTs: true,
   peerDeps: ['projen'],
   deps: [
     '@graphql-codegen/plugin-helpers',
     // lowers and pins the version to mitigate
     // https://github.com/dotansimha/graphql-code-generator/issues/7519
-    'graphql@16.3.0',
+    'graphql',
   ],
   depsUpgrade: false,
   releaseToNpm: true,
